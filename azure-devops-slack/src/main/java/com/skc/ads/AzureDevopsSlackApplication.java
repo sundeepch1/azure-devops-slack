6a1a85d960f016f20706cff430d2f824743006c3
sundeepch1 <sundeep.chaurasiya@gmail.com>
2022-11-23 19:37:04 +0530

    first commit

---
+++ azure-devops-slack/src/main/java/com/skc/ads/AzureDevopsSlackApplication.java
@@ -0,0 +1,13 @@
+package com.skc.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDevopsSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDevopsSlackApplication.class, args);
+	}
+
+}
---
+++ azure-devops-slack/src/test/java/com/skc/ads/AzureDevopsSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skc.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDevopsSlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
